2270c5d152ab80c1a3ed249706a33b8a26b29be3
carlaranas14 <carlaranas01@gmail.com>
2024-10-23 18:52:05 +0800

    admin login

---
+++ admin/admin_login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+</head>
+<body>
+    <div class="header">
+        <img src="" alt="">
+        <p>happy smile dental clinic</p>
+        <i>login</i>
+        <i>public home</i>
+    </div>
+
+    <form action="">
+        <h1>admin area</h1>
+        <label for="email"></label>
+        <input type="emai" name="email">
+
+        <span class="media"></span>
+        <label for="password"></label>
+        <input type="password" name="password">
+
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
